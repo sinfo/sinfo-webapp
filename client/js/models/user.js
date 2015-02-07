02653b1a98fd8ee90242a852a6ef0ea8fa5e214b
Francisco Baio Dias <xicombd@gmail.com>
2015-02-07 14:13:11 +0000

    Added User model and made Me derived from it

---
+++ client/js/models/user.js
@@ -0,0 +1,34 @@
+var AmpModel = require('ampersand-model');
+var AmpState = require('ampersand-state');
+
+var Job = AmpState.extend({
+  props: {
+    startup: 'boolean',
+    internship: 'boolean',
+    start: 'date',
+  }
+});
+
+module.exports = AmpModel.extend({
+  type: 'user',
+  props: {
+    id: 'string',
+    name: 'string',
+    img: 'string',
+    mail: 'string',
+    role: 'string',
+    area: 'string',
+    skills: 'array',
+  },
+  children: {
+    job: Job,
+  },
+  derived: {
+    background: {
+      deps: ['img'],
+      fn: function () {
+        return 'background-image:url('+this.img+');';
+      }
+    },
+  },
+});
