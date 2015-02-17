ed98518ed313e1167a42a1637c3efa0e94e98049
Francisco Baio Dias <xicombd@gmail.com>
2015-02-17 16:48:38 +0000

    Working on session users list

--- client/js/views/users/view.js
+++ client/js/views/users/view.js
@@ -5,16 +5,8 @@ var templates = require('client/js/templates');
 module.exports = View.extend({
   template: templates.partials.users.view,
   bindings: {
-    'model.name': '[data-hook~=name]',
-    'model.background': {
-      type: 'attribute',
-      hook: 'img',
-      name: 'style'
-    },
-    'model.viewUrl': {
-      type: 'attribute',
-      hook: 'action-view',
-      name: 'href'
-    }
+    'model.name': { type: 'attribute', hook: 'action-view', name: 'title' },
+    'model.img': { type: 'attribute', hook: 'img', name: 'src' },
+    'model.viewUrl': { type: 'attribute', hook: 'action-view', name: 'href' }
   }
 });
