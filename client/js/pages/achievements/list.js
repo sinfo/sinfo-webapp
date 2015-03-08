83302df7ee4826f232f91903ca75fb9edd49ee48
JGAntunes <j.goncalo.antunes@gmail.com>
2015-03-08 04:59:42 +0000

    added top SINFOlicos

--- client/js/pages/achievements/list.js
+++ client/js/pages/achievements/list.js
@@ -1,6 +1,19 @@
 /*global app, alert*/
 var AchievementsArea = require('client/js/views/achievements/area');
+var TopView = require('client/js/pages/users/top');
 
 module.exports = AchievementsArea.extend({
-  pageTitle: 'Achievements'
+  pageTitle: 'Achievements',
+  subviews: {
+  	top: {
+      container: '[data-hook=top-users] div',
+      parent: this,
+      prepareView: function (el) {
+        var self = this;
+        return new TopView({
+          el: el,
+        });
+      }
+  	}
+  }
 });
