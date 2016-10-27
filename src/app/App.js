1a9a232ae05d80df05057205e77fa34e430ab30d
JMoura <joao_moura_@hotmail.com>
2016-10-27 15:38:10 +0100

    Updated Header style

--- src/app/App.js
+++ src/app/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Head from './components/Head'
 import Carousel from './components/Carousel'
+import Events from './components/Events'
 
 class App extends Component {
   componentDidMount () {
     // We need to require css here in order for it to be isomorphic
     require('bootstrap/dist/css/bootstrap.css')
     require('./css/App.css')
+    require('./css/Header.css')
     require('./css/react-boostrap-carousel.css')
     // logo = require('./images/logo.svg')
   }
@@ -19,12 +21,7 @@ class App extends Component {
         <Head />
         <Header />
         <Carousel />
-        <div className='App-header'>
-          <h2>Welcome to React</h2>
-        </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Events />
         <Link to='/test'> BATATA POWER</Link>
         <Footer />
       </div>
