e6ca4ca56757ee0449f9627f216faf36cb3faf14
CarlosFaria94 <carlosfaria94@gmail.com>
2016-11-21 18:39:48 +0000

    Social links correctness

--- src/app/components/members/Member.js
+++ src/app/components/members/Member.js
@@ -4,17 +4,25 @@ import { Col } from 'react-bootstrap'
 export default class Member extends Component {
   render () {
     const style = { backgroundImage: 'url(' + this.props.img + ')' }
-    const email = 'mailto:' + this.props.mail
-    const twitterLink = 'https://twitter.com/' + this.props.twitter
-    const gitHubLink = 'https://github.com/' + this.props.github
-
+    let mailLink
+    if (typeof this.props.mail !== 'undefined') {
+      mailLink = <a target='_blank' href={'mailto:' + this.props.mail} className='social-icon fa fa-envelope-o' />
+    }
+    let twitterLink
+    if (typeof this.props.twitter !== 'undefined') {
+      twitterLink = <a target='_blank' href={'https://twitter.com/' + this.props.twitter} className='social-icon fa fa-twitter' />
+    }
+    let gitHubLink
+    if (typeof this.props.github !== 'undefined') {
+      gitHubLink = <a target='_blank' href={'https://github.com/' + this.props.github} className='social-icon fa fa-github' />
+    }
     return (
       <Col sm={6} md={2} lg={2} key={this.props.index} className='member'>
         <div className='name'>{this.props.name}</div>
         <div className='img' style={style} />
-        <a target='_blank' href={email} className='social-icon fa fa-envelope-o' />
-        <a target='_blank' href={twitterLink} className='social-icon fa fa-twitter' />
-        <a target='_blank' href={gitHubLink} className='social-icon fa fa-github' />
+        {mailLink}
+        {twitterLink}
+        {gitHubLink}
       </Col>
     )
   }
