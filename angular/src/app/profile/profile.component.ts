0b22125703e263196841bc632e555dad7c9fb808
CarlosFaria94 <carlosfaria94@gmail.com>
2017-10-10 22:12:26 +0100

    Apply JavaScript Standard Style

--- angular/src/app/profile/profile.component.ts
+++ angular/src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor () { }
 
-  ngOnInit() {
+  ngOnInit () {
   }
 
 }
