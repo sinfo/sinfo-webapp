e24d43df2b376145671d5435f9416707efdafebe
fchamicapereira <fchamicapereira@gmail.com>
2017-11-16 00:13:21 +0000

    now pulling sponsors from deck

--- Ventcamp_HTML/assets/js/fetch.js
+++ Ventcamp_HTML/assets/js/fetch.js
@@ -2,6 +2,7 @@ var  deck = 'https://deck.sinfo.org:443/api/';
 
 $(document).on('ready', function () {
   fetchFromDeck('members','event=25-sinfo&&participations=true', processMember);
+  fetchFromDeck('companies','event=24-sinfo&&participations=true', processSponsors);
 });
 
 function fetchFromDeck(field, params, processDataFromDeck) {
@@ -10,7 +11,6 @@ function fetchFromDeck(field, params, processDataFromDeck) {
   request.responseType = 'json';
   request.send();
   request.onload = function() {
-    console.log(request.response)
     request.response.forEach(function(el){
       processDataFromDeck(el);
     });
@@ -49,5 +49,39 @@ function processMember(member) {
       socialNav += `<li><a href='https://github.com/${member["github"]}'><span class="fa fa-github"></span></a></li>`;
 
     return socialNav;
+  }
+}
+
+function processSponsors(sponsor) {
+  var platinumPerLine = 4;
+
+  /* diamond */
+  if (sponsor.advertisementLvl == 'exclusive') {
+    $("#sponsors-diamond").append(
+      `<div class="sponsor big"><img src=${sponsor.img} alt=""></div>`
+    );
+  }
+
+  /* platinum */
+  else if (sponsor.advertisementLvl == 'max') {
+    var loadedPlatinum = $(".platinum");
+    var lastPlatinum = loadedPlatinum[loadedPlatinum.length - 1];
+
+    /* if this div has fewer than 6 sponsors */
+    if (lastPlatinum && $(lastPlatinum).children().length < platinumPerLine) {
+
+      $(lastPlatinum).append(
+        `<div class="sponsor"><img src=${sponsor.img} alt=""></div>`
+      );
+    }
+
+    /* else, create new div */
+    else {
+      $('#sponsors > div').append(
+        `<div class="sponsors platinum">
+        <div class="sponsor"><img src=${sponsor.img} alt=""></div>
+        </div>`
+      );
+    }
   }
 }
